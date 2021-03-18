3ecb4be58cf87ab1938a804a9efbd751295db268
Andrea Pérez <andreaa.perz@gmail.com>
2021-03-17 19:03:45 -0600

    práctica con estilos

--- lib/src/pages/homepage_alternativo.dart
+++ lib/src/pages/homepage_alternativo.dart
@@ -1,15 +1,48 @@
 import 'package:flutter/material.dart';
  
 class HomePageAlternativo extends StatelessWidget {
-
   final opciones = [
-    {"title": "Uno", "description": "Description 1", "leading": Icons.card_travel_rounded, "trailing": Icons.adjust_rounded, "estilo": Colors.green.shade100},
-    {"title": "Dos", "description": "Description 2", "leading": Icons.airplanemode_active_rounded, "trailing": Icons.arrow_forward_ios_rounded, "estilo": Colors.green.shade200},
-    {"title": "Tres", "description": "Description 3", "leading": Icons.airport_shuttle_rounded, "trailing": Icons.arrow_right_rounded, "estilo": Colors.green.shade500},
-    {"title": "Cuatro", "description": "Description 4", "leading": Icons.anchor_rounded, "trailing": Icons.check_circle_outline_rounded, "estilo": Colors.green.shade600},
-    {"title": "Cinco", "description": "Description 5", "leading": Icons.beach_access_rounded, "trailing": Icons.done_outline_rounded, "estilo": Colors.green.shade800}
+    {
+      "title": "Uno",
+      "subtitle": "Elemento 1",
+      "leading": Icons.card_travel_rounded,
+      "trailing": Icons.adjust_rounded,
+      "estilost": Colors.green.shade900,
+      "estilossub": Colors.grey,
+    },
+    {
+      "title": "Dos",
+      "subtitle": "Elemento 2",
+      "leading": Icons.airplanemode_active_rounded,
+      "trailing": Icons.arrow_forward_ios_rounded,
+      "estilost": Colors.green.shade800,
+      "estilossub": Colors.grey,
+    },
+    {
+      "title": "Tres",
+      "subtitle": "Elemento 3",
+      "leading": Icons.airport_shuttle_rounded,
+      "trailing": Icons.arrow_right_rounded,
+      "estilost": Colors.green.shade700,
+      "estilossub": Colors.grey,
+    },
+    {
+      "title": "Cuatro",
+      "subtitle": "Elemento 4",
+      "leading": Icons.anchor_rounded,
+      "trailing": Icons.check_circle_outline_rounded,
+      "estilost": Colors.green.shade600,
+      "estilossub": Colors.grey,
+    },
+    {
+      "title": "Cinco",
+      "subtitle": "Elemento 5",
+      "leading": Icons.beach_access_rounded,
+      "trailing": Icons.done_outline_rounded,
+      "estilost": Colors.green.shade500,
+      "estilossub": Colors.grey,
+    },
   ];
-
 
   @override
   Widget build(BuildContext context) {
@@ -38,13 +71,29 @@ class HomePageAlternativo extends StatelessWidget {
     return opciones.map((item) {
       return Column(
         children: [
-          ListTile(
-            title: Text((item['title']), style: TextStyle(color: (item['estilo']),  fontSize: 20.0, fontWeight: FontWeight.w500)),
-            subtitle: Text(item['description'], style: TextStyle(color: Colors.grey.shade700, fontSize: 15.0, fontWeight: FontWeight.w200)),
-            leading: Icon(item['leading']),
+          Container(
+            margin: const EdgeInsets.fromLTRB(5, 10, 5, 5),
+            alignment: Alignment.bottomRight,
+          child: ListTile(
+            title: Text((item['title']), style: TextStyle(color: (item['estilost']), fontSize: 20.0, fontWeight: FontWeight.w500)),
+            subtitle: Text((item['subtitle']), style: TextStyle(color: (item['estilossub']), fontSize: 15.0, fontWeight: FontWeight.w300)),
+            leading: Container(
+              padding: EdgeInsets.all(5.8),
+              child: Icon(item['leading'])),
             trailing: Icon(item['trailing']),
-            onTap: (){},
+            onTap: () {},
           ),
+          decoration: BoxDecoration(
+            color: Colors.teal.shade100,
+            borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(8.0),
+            topRight: Radius.circular( 8.0),
+            bottomLeft: Radius.circular( 8.0),
+            bottomRight: Radius.circular( 8.0),
+        )
+        ),
+
+        ),
           Divider()
         ],
       );
